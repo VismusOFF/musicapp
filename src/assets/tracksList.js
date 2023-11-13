3abc1240e4eb9bcfd09acd49f8e2ceb0c0717810
John Doe <johndoe@example.com>
2023-11-13 20:05:00 +0300

    Reack+Vite

---
+++ src/assets/tracksList.js
@@ -0,0 +1,200 @@
+export default [
+    {
+      id: 1,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/250/what-i-say-1668733254-7bXF48DYkS.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/154/1000x0/no-way-back-1656410436-lmCSrM15Fp.jpg",
+      duration: 215,
+      title: "No Way Back",
+      artists: "Custody",
+    },
+    {
+      id: 2,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/154/no-way-back-1656410438-DYBZU8U2rN.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/113/1000x0/falling-1649412036-lkoB5hiJq8.jpg",
+      duration: 159,
+      title: "Falling",
+      artists: "Rival, CRVN",
+    },
+    {
+      id: 3,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/022/looking-for-more-1633705235-Ivt5PS0zSF.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/022/1000x0/looking-for-more-1633705232-L8MteCTyyQ.jpg",
+      duration: 244,
+      title: "Looking For More",
+      artists: "Koven",
+    },
+    {
+      id: 4,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/006/what-you-like-1631271635-cC2vMe487N.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/006/1000x0/what-you-like-1631271631-1nbDNE73Qu.jpg",
+      duration: 158,
+      title: "What You Like",
+      artists: "Curbi",
+    },
+    {
+      id: 5,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/885/dead-of-night-1609236037-9MfpB6VEhX.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/885/1000x0/dead-of-night-1609236028-MO9yxOe5HC.jpg",
+      duration: 233,
+      title: "Dead of Night",
+      artists: "if found",
+    },
+    {
+      id: 6,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/853/phenomenon-1605895230-EleosL4jlP.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/853/1000x0/phenomenon-1605895227-fAhxMTrRpG.jpg",
+      duration: 176,
+      title: "Phenomenon",
+      artists: "VinDon",
+    },
+    {
+      id: 7,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/838/1602924116_i3Tn2EkNdU_Rival---Lonely-Way-ft.-Caravn-NCS-Release.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/838/1000x0/1602924116_v2O3UoVY11_Artwork.jpg",
+      duration: 224,
+      title: "Lonely Way",
+      artists: "Rival, Caravn",
+    },
+    {
+      id: 8,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/691/new-style-1586959962-Qyaa8hmvgv.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/691/1000x0/new-style-1586959956-pTXtBw9yzO.jpg",
+      duration: 217,
+      title: "New Style",
+      artists: "Droptek",
+    },
+    {
+      id: 9,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/671/where-we-started-feat-jex-1586959343-ugPNLDQsMI.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/671/1000x0/where-we-started-feat-jex-1586959339-BqrS2PV0TI.jpg",
+      duration: 222,
+      title: "Where We Started",
+      artists: "Lost Sky, Jex",
+    },
+    {
+      id: 10,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/676/volt-1586959481-7oo0fw99Vt.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/676/1000x0/volt-1586959457-zDcTmbdEOq.png",
+      duration: 183,
+      title: "VOLT",
+      artists: "Modern Revolt",
+    },
+    {
+      id: 11,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/649/war-zone-ft-mime-1586958862-uOdchVMspI.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/649/1000x0/war-zone-ft-mime-1586958856-RK8C6g2w4R.jpg",
+      duration: 183,
+      title: "War Zone",
+      artists: "Unknown Brain",
+    },
+    {
+      id: 12,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/616/vision-1586958106-w6RKce6Cz4.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/616/1000x0/vision-1586958102-Zzy5Gf1SKm.jpg",
+      duration: 234,
+      title: "Vision",
+      artists: "Lost Sky",
+    },
+    {
+      id: 13,
+      src: "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/577/cradles-1586957268-RxHgjX9e14.mp3",
+      preview:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/577/100x100/cradles-1586957231-QpMwffYL5H.png",
+      duration: 210,
+      title: "Cradles",
+      artists: "Sub Urban",
+    },
+    {
+        id: 14,
+        src: "https://patefon.org/load/1171590583/bilbordy_-_KHolodnoe_dykhanie_(patefon.org).mp3",
+        preview:
+          "https://muzlada.net/uploads/mini/poster-full/e3/178060cb4fc6ac17372b4ebfda6e26.webp",
+        duration: 154,
+        title: "Холодное дыхание",
+        artists: "Билборды",
+      },
+      {
+        id: 15,
+        src: "https://patefon.org/load/298105593/bilbordy_-_Ostrye_lezviya_(patefon.org).mp3",
+        preview:
+          "https://avatars.yandex.net/get-music-content/5678677/8c69342f.a.21154527-1/150x150",
+        duration: 163,
+        title: "Острые лезвия",
+        artists: "Билборды",
+      },
+      {
+        id: 16,
+        src: "https://patefon.org/load/1190503477/bilbordy_-_Risuyu_krovyu_(patefon.org).mp3",
+        preview:
+          "https://avatars.yandex.net/get-music-content/5559490/985cbea5.a.18575090-1/150x150",
+        duration: 155,
+        title: "Рисую кровью",
+        artists: "Билборды",
+      },
+      {
+        id: 17,
+        src: "https://patefon.org/load/1503795595/bilbordy_-_Vmesto_poslednejj_strochki_(patefon.org).mp3",
+        preview:
+          "https://avatars.yandex.net/get-music-content/4581417/e8c8afc9.a.17497205-1/150x150",
+        duration: 147,
+        title: "Вместо последней строчки",
+        artists: "Билборды",
+      },
+      {
+        id: 18,
+        src: "https://patefon.org/load/1195825055/bilbordy_-_Ischezayu_(patefon.org).mp3",
+        preview:
+          "https://avatars.yandex.net/get-music-content/5631481/8da94865.a.20132435-1/150x150",
+        duration: 146,
+        title: "Исчезаю",
+        artists: "Билборды",
+      },
+      {
+        id: 19,
+        src: "https://patefon.org/load/1756987305/bilbordy_-_Alye_cvety_(patefon.org).mp3",
+        preview:
+          "https://avatars.yandex.net/get-music-content/6296749/b19cdbfd.a.22362237-1/150x150",
+        duration: 165,
+        title: "Алые цветы",
+        artists: "Билборды",
+      },
+      {
+        id: 20,
+        src: "https://patefon.org/load/1114716040/Korol_i_SHut_-_Lesnik_(patefon.org).mp3",
+        preview:
+          "https://avatars.yandex.net/get-music-content/7548376/a8ed253b.a.7018993-3/150x150",
+        duration: 191,
+        title: "Лесник",
+        artists: "Король и шут",
+      },
+      {
+        id: 21,
+        src: "https://patefon.org/load/1383383956/Korol_i_SHut_-_Kukla_kolduna_(patefon.org).mp3",
+        preview:
+          "https://avatars.yandex.net/get-music-content/163479/b3e3efc4.a.7019257-3/150x150",
+        duration: 203,
+        title: "Кукла колдуна",
+        artists: "Король и шут",
+      },
+      {
+        id: 22,
+        src: "https://patefon.org/load/903537000/Korol_i_SHut_-_Eli_myaso_muzhiki_(patefon.org).mp3",
+        preview:
+          "https://avatars.yandex.net/get-music-content/163479/b3e3efc4.a.7019257-3/150x150",
+        duration: 134,
+        title: "Ели мясо мужики",
+        artists: "Король и шут",
+      },
+  ];
