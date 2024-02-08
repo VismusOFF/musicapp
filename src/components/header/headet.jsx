d12a3b2fafdbed020723dfe467671dc8fcdf48e9
John Doe <johndoe@example.com>
2024-02-08 21:19:58 +0300

    AddButtonThemes

--- src/components/header/headet.jsx
+++ src/components/header/headet.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import mainIcon from './main.png';
 import style from './header.module.scss';
-import { IconButton } from "@mui/material";
-import { DarkModeIcon, LightModeIcon } from '@mui/icons-material/DarkMode';
-import { light } from '@mui/material/styles/createPalette';
 
 const Header = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
+  const headerStyle = {
+    backgroundColor: theme === 'dark' ? '#cc0000' : '#4a1457',
+  };
 
   return (
-    <div className={style.header}>
-      <img src={mainIcon} alt="site icon" style={{ marginLeft: '10px', width: '50px', height: '50px' }} />
-      <h1 style={{ marginLeft: '10px', color: 'white' }}>ZXCmusic</h1>
+    <div
+      className={`${style.header} ${theme === 'dark' ? style.darkTheme : ''}`}
+      style={headerStyle}
+    >
+      <img
+        src={mainIcon}
+        alt="site icon"
+        style={{ marginLeft: '10px', width: '50px', height: '50px' }}
+      />
+      <h1 style={{ marginLeft: '10px' }}>ZXCmusic</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
     </div>
   );
 };
